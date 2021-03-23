e4fbe6683e07651b58d80d7ee1138cd574235a56
deepak.s@fingent.com <deepak.s@fingent.com>
2021-03-23 10:38:51 +0530

    Login View Designs

--- RCG.WPF/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ RCG.WPF/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -55,7 +55,7 @@ namespace RCG.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RCG.WPF;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RCG.WPF;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
