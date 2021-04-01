cbdfe0bf8a9e312c756ed2f3a239ebade66c8c55
mehaboob.ali <mehaboob.ali@fingent.com>
2021-04-01 18:57:08 +0530

    navigation bar design changes

--- RCG.WPF/ViewModels/MainViewModel.cs
+++ RCG.WPF/ViewModels/MainViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 using RCG.CoreApp.Interfaces.User;
 using RCG.WPF.Commands;
+using RCG.WPF.Services;
+using RCG.WPF.State.Accounts;
 using RCG.WPF.State.Authenticators;
 using RCG.WPF.State.Navigators;
 using RCG.WPF.ViewModels.Factories;
@@ -35,24 +38,38 @@ namespace RCG.WPF.ViewModels
         private readonly INavigator _navigator;
         private readonly IAuthenticator _authenticator;
         private readonly IUserService _authenticationService;
-
+        private readonly IDialogService _dialogService;
+        private readonly UserSetupEditViewModel _userSetupEditViewModel;
+        private readonly IUserStore _userStore;
         public bool IsLoggedIn => _authenticator.IsLoggedIn;
         public ViewModelBase CurrentViewModel => _navigator.CurrentViewModel;
 
         public ICommand UpdateCurrentViewModelCommand { get; }
+        public ICommand UserSettingsCommand { get; }
 
-        public MainViewModel(INavigator navigator, IProductManagerViewModelFactory viewModelFactory, IAuthenticator authenticator, IUserService authenticationService)
+        public MainViewModel(
+            INavigator navigator,
+            IProductManagerViewModelFactory viewModelFactory,
+            IAuthenticator authenticator,
+            IUserService authenticationService,
+            IDialogService dialogService,
+            UserSetupEditViewModel userSetupEditViewModel,
+            IUserStore userStore)
         {
             _navigator = navigator;
             _viewModelFactory = viewModelFactory;
             _authenticator = authenticator;
             _authenticationService = authenticationService;
+            this._userStore = userStore;
+            this._dialogService = dialogService;
+            this._userSetupEditViewModel = userSetupEditViewModel;
 
             _navigator.StateChanged += Navigator_StateChanged;
             _authenticator.StateChanged += Authenticator_StateChanged;
 
             UpdateCurrentViewModelCommand = new UpdateCurrentViewModelCommand(navigator, _viewModelFactory);
-            //UpdateCurrentViewModelCommand.Execute(ViewType.Login);
+            this.UserSettingsCommand = new RelayCommand(a => this.UserSettings());
+            ////UpdateCurrentViewModelCommand.Execute(ViewType.Login);
 
             ////WindowHeight = Convert.ToInt32(Resource.HeightProductsView);
             ////WindowWidth = Convert.ToInt32(Resource.WidthProductsView);
@@ -71,9 +88,40 @@ namespace RCG.WPF.ViewModels
 
         }
 
+        private string _loggedUserName;
+        private string _loggedUserFirstLetter;
+
+        public string LoggedUserName
+        {
+            get { return _loggedUserName; }
+            set
+            {
+                _loggedUserName = value;
+                this.LoggedUserFirstLetter = value[0].ToString().ToUpper();
+                this.OnPropertyChanged("LoggedUserName");
+            }
+        }
+
+
+        public string LoggedUserFirstLetter
+        {
+            get { return _loggedUserFirstLetter; }
+            set { _loggedUserFirstLetter = value; this.OnPropertyChanged("LoggedUserFirstLetter"); }
+        }
+
+        private void UserSettings()
+        {
+            _userSetupEditViewModel.Title = "User Settings";
+            this._dialogService.OpenDialog(_userSetupEditViewModel);
+        }
+
         private void Authenticator_StateChanged()
         {
             OnPropertyChanged(nameof(IsLoggedIn));
+            if (IsLoggedIn)
+            {
+                this.LoggedUserName = this._userStore.IsAuth.Name;
+            }
         }
 
         private void Navigator_StateChanged()
