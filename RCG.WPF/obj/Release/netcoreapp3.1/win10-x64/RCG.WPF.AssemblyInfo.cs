8e75fbb49608076ef65910989fa8b908c93edb8f
deepak.s@fingent.com <deepak.s@fingent.com>
2021-04-27 15:41:41 +0530

    Staging merges

--- RCG.WPF/obj/Release/netcoreapp3.1/win10-x64/RCG.WPF.AssemblyInfo.cs
+++ RCG.WPF/obj/Release/netcoreapp3.1/win10-x64/RCG.WPF.AssemblyInfo.cs
@@ -11,8 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Product Manager")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Royal Chain Group")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Royal Chain Group")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product Manager")]
